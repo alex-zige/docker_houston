cad7864432e39646ee7b664408c08a14403e7b62
alex-zige <alex.zige@gmail.com>
2016-04-10 16:16:48 +1200

    change

--- lib/docker_houston.rb
+++ lib/docker_houston.rb
@@ -1,5 +1,8 @@
 require "docker_houston/version"
-
+require 'capistrano/all'
+require 'capistrano/git'
+require 'docker_houston/railtie'
+require 'docker_houston/capistrano.rb'
 module DockerHouston
   # Your code goes here...
 end
--- lib/tasks/new.rake
+++
@@ -1,7 +0,0 @@
-namespace :aaa do
-  task :bbb do
-    invoke "deploy:print_config_variables" if fetch(:print_config_variables, false)
-    invoke "deploy:check"
-    invoke "deploy:set_previous_revision"
-  end
-end
